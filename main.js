04a094c1fea141cb0227839dcac5cff3f39dbfdf
heero7 <kevin7.mudia@gmail.com>
2025-03-31 22:28:07 -0500

    create file, add tasks, but no write yet

--- main.js
+++ main.js
@@ -7,12 +7,95 @@ import { parseArgs } from "jsr:@std/cli/parse-args";
  * */
 async function checkForFile() {
   try {
-    _ = await Deno.lstat("./tasks_current_user.json");
+    await Deno.lstat("./tasks_current_user.json");
     return true;
   } catch (err) {
     console.warn("[TaskCLI:warn] User File does not exist!");
+    console.error("[TaskCLI:err]", err);
     return false;
   }
+}
+
+let tasks = new Map();
+function addTask() {
+    const taskName = prompt("Please enter a name for this task!");
+    // Limit to only allow creating tasks in the current year.
+    // Only show options for the next day.
+    // So format looks like MM DD
+    // Today for example 3, 31 is not valid. The next value
+    // should be 4 01
+    const currentDate = new Date();
+    currentDate.setHours(0,0,0,0);
+    currentDate.setDate(currentDate.getDate() + 1);
+    const month = currentDate.getMonth() + 1; // month is 0 based
+    const day = currentDate.getDate();
+    const year = currentDate.getFullYear();
+    const dueDatePrompt = 
+        `When is this task due? Earliest allowed due date task is: ${month}-${day}
+        \nEnter a month:`;
+    const taskDueDateMonth = prompt(dueDatePrompt);
+    const taskDueDateDay = prompt("Enter a day (1-31)");
+
+    // Validate the day
+    // It cannot be less than currentDate and has to be valid
+    console.log('Parsing date...');
+    const m = parseInt(taskDueDateMonth);
+    const d = parseInt(taskDueDateDay);
+    console.log('End Parsing date...');
+
+    if (m == NaN) {
+        console.error("[TaskCLI:error] the number entered was not a number: ", taskDueDateMonth);
+        return;
+    }
+
+    if (d == NaN) {
+        console.error("[TaskCLI:error] the number entered was not a number: ", taskDueDateDay);
+        return;
+    }
+
+    try {
+        // this is now the date parsed date. This can't be less then current date.
+        console.log('Validating date...');
+        const dueDate = new Date(`${m}/${d}/${year}`);
+        const dateString = dueDate.toLocaleDateString("en-US", {
+          year: "numeric",
+          month: "2-digit",
+          day: "numeric"
+        });
+
+        if (dateString < currentDate) {
+            console.error("[TaskCLI:error] This date is in the past and won't work!");
+            return;
+        }
+
+        console.log("[TaskCLI] Able to create a good due date!");
+        const task = {
+            name: taskName,
+            dueDate: dateString,
+            status: 0
+        };
+
+        if (tasks.get(task.dueDate)) {
+            tasks.get(task.dueDate).push(task);
+        } else {
+            // create a new key value
+            console.log('did not exist, now adding!');
+            tasks.set(task.dueDate, [task]);
+        }
+    } catch (err) {
+        console.error("[TaskCLI:error] problem trying to set due date for task: ", err);
+    }
+}
+
+function showTasks() {
+    console.log('*********** Tasks ***********');
+    console.log('Tasks?', tasks.size);
+    tasks.forEach((value, key) => {
+        console.log(`Tasks for ${key}`);
+        console.log('Number of tasks ', value.length);
+        value.map(v => console.log('\t\t' + v.name));
+    });
+    console.log('*********** End Tasks ***********');
 }
 
 // If this fails, we can return just false.
@@ -44,6 +127,7 @@ async function loadTasks() {
     const tasks = await Deno.open("./tasks_current_user.json");
     status.fileLoaded = true;
     status.data = tasks;
+    console.log("Successfully found a tasks file.");
   }
   return status;
 }
@@ -73,10 +157,15 @@ const dataLoad = await loadTasks();
 let cliActive = dataLoad.fileLoaded;
 const data = dataLoad.data;
 
+// todo: only show the main prompt if asked to. because then we can't see anything
 while (cliActive) {
   const selected = prompt(mainMenuPrompt);
   if (selected == "8") {
     cliActive = false;
     console.log("Goodbye!");
+  } else if (selected == "1") {
+      addTask();
+  } else if (selected == "4") {
+      showTasks();
   }
 }
